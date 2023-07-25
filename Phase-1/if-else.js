4dc8cd9a65fe5f0be9e0fc618aaaa17461ed29c9
Hafifsyah Rifaldi <hafifsyahr@gmail.com>
2023-07-25 14:56:11 +0700

    if-else

---
+++ Phase-1/if-else.js
@@ -0,0 +1,10 @@
+let nilai = 30;
+// console.log(nilai >= 50); //tergantung si nilainya, hasilnya antara true or false
+if (nilai >= 50) {
+    // true or false
+    console.log(`Lulus`);
+} else if (nilai >= 40) {
+    console.log(`Tidak Lulus`);
+} else {
+    console.log(`Tidak Lulus BANGET!!`);
+}

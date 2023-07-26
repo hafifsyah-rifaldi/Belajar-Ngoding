af5e4571cb55fd5f44f5653f7ed0d577f3dacbb5
Hafifsyah Rifaldi <hafifsyahr@gmail.com>
2023-07-26 20:42:21 +0700

    Quiz 1

---
+++ Phase-1/1quiz-while.js
@@ -0,0 +1,74 @@
+/* Simpelnya modulus operator adalah sisa bagi
+Misal 3 / 2 hasilnya akan koma, tetap jika 3 % 2 hasilnya akan 1
+maka apapun angkanya kalau di modulus apapun itu angkanya juga dan hasilnya adalah 0, maka itu kelipatannya
+    Contoh n % 3 === 0 // maka ini adalah kelipatan 3
+    Contoh n % 5 === 0 // maka ini adalah kelipatan 5
+    Contoh n % 2 === 0 // maka ini adalah kelipatan 2, dan 2 kebetulan bisa menentukan ganjil atau genap
+*/
+/* Contoh belajar menggabungkan dengan if else
+let i = 0;
+
+while (i < 10) {
+    // disini kita mencari ganjil atau genap
+    if (i % 2 === 0) {
+        console.log(`${i} adalah genap`);
+    } else {
+        console.log(`${i} adalah ganjil`);
+    }
+    i++;
+}
+ */
+/*  Contoh problem fizzbuzz
+for (let i = 1; i < 50; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0 && i % 5 !== 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0 && i % 3 !== 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+} */
+/* Contoh break continue
+for (let i = 0; i < 10; i++) {
+    if (i === 7) {
+        continue;
+        // break;
+    }
+    console.log(i);
+} */
+
+/* 1. Melakukan Looping Menggunakan While
+Problem
+Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript dengan menggunakan syntax while.
+Untuk membuat tantangan ini lebih menarik, kamu juga diminta untuk membuat suatu looping yang menghitung maju dan
+menghitung mundur. Jangan lupa tampilkan di console juga judul 'LOOPING PERTAMA' dan 'LOOPING KEDUA'.
+tampilkan output yang menunjukan kalau looping itu maju pada looping pertama dan mundur pada looping kedua
+ */
+
+/* 1. Melakukan Looping Menggunakan For
+Problem
+Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript dengan menggunakan syntax while.
+Untuk membuat tantangan ini lebih menarik, kamu juga diminta untuk membuat suatu looping yang menghitung maju dan
+menghitung mundur. Jangan lupa tampilkan di console juga judul 'LOOPING PERTAMA' dan 'LOOPING KEDUA'.
+tampilkan output yang menunjukan kalau looping itu maju pada looping pertama dan mundur pada looping kedua
+ */
+
+/* 3. Angka Ganjil dan Genap
+Hint: kamu akan menggunakan kondisional juga di kasus ini.
+
+Problem
+Buatlah sebuah perulangan 1 - 100 dengan pertambahan counter sebanyak 1
+Di dalam perulangan, periksa setiap angka counter:
+Apabila angka counter adalah angka genap, tuliskan GENAP
+Apabila angka counter adalah angka ganjil, tuliskan GANJIL
+ */
+
+/* 4. counter kelipatan
+Buatlah 3 perulangan baru dari 1 - 100, dengan pertambahan counter sebesar 2, 5, dan 9.
+Pada 3 perulangan baru ini periksa setiap angka counter:
+
+Apabila bukan kelipatan yang ditentukan tidak perlu menuliskan apa-apa
+Apabila angka counter adalah kelipatan 3 dengan pertambahan 2, kelipatan 6 dengan pertambahan 5, dan kelipatan 10 dengan pertambahan 9, tuliskan:
+"3 kelipatan 3"dan seterusnya. */
